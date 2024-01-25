fd44d8f3c140a53ccac2276563848c0546e741dd
Victor Langat <provicml@gmail.com>
2024-01-25 14:38:51 +0300

    pycodestyle fix

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-function to return a tuple of size two containing a 
+function to return a tuple of size two containing a
 start index and an end index corresponding to the range of
 indexes to return in a list for those particular pagination
 parameters"""
