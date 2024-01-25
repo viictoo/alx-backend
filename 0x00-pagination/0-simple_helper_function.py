73a4feef29de6568ff077e3a0ab05d34a563a667
Victor Langat <provicml@gmail.com>
2024-01-25 13:45:47 +0300

    simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -8,13 +8,6 @@ from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """_summary_
-
-    Args:
-        page (int): _description_
-        page_size (int): _description_
-
-    Returns:
-        Tuple[int, int]: _description_
+    """Simple helper function
     """
     return ((page - 1) * page_size, page * page_size)
