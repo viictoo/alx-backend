e859e08855c014b124d94d7421eb7a6746324c0d
Victor Langat <provicml@gmail.com>
2024-02-06 18:41:46 +0300

    test

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-"""basic flask app with  Parametrized templates"""
+"""module for flask app and babel"""
+
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
 class Config:
-    """config settings for babel i18n
-    Args:
-        object (obj): instance of a class
-    """
+    """Configuration class for Flask application."""
+
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -21,20 +20,11 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
-    """determine the best match with our
-    supported languages
-
-    Returns:
-        str: best language match
-        """
+    """determines the locale to be used"""
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-@app.route('/', strict_slashes=False)
-def root():
-    """base route
-
-    Returns:
-        template: page presentation
-    """
-    return render_template('3-index.html')
+@app.route("/")
+def hello():
+    """renders 0-index.html"""
+    return render_template("3-index.html")
