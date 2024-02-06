8942c7b592f252545c05f9246cc2a90881a12eac
Victor Langat <provicml@gmail.com>
2024-02-06 18:40:00 +0300

    refactor app config comments

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -30,9 +30,6 @@ def get_locale():
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-# babel.init_app(app, locale_selector=get_locale)
-
-
 @app.route('/', strict_slashes=False)
 def root():
     """base route
